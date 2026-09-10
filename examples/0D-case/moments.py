8dc31b9ec8c99295a298a0ccdf52ed02471c1acc
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2026-09-10 17:32:29 +0300

    Implement concentration-dependent growth rate in the reference solution Python script
    Also output and plot saturation concentration

--- examples/0D-case/moments.py
+++ examples/0D-case/moments.py
@@ -25,15 +25,39 @@ def calculate_T(t):
     return T_curve(t)
 
 
-def calculate_G(T, L):
-    return kG * np.exp(-Eg / (R * T)) * np.pow(1.0 + alphaG * L, Lg)
+def calculate_G(T, L, C):
+    C_sat = calculate_C_sat(T)
+    dC = np.clip(C - C_sat)
+    return (
+        kG
+        * np.exp(-Eg / (R * T))
+        * np.pow(1.0 + alphaG * L, Lg)
+        * np.pow(dC, g)
+    )
 
 
-def calculate_dG_dL(T, L):
+def calculate_dG_dL(T, L, C):
     if Lg <= 0:
         return 0
+    C_sat = calculate_C_sat(T)
+    dC = np.clip(C - C_sat)
     return (
-        kG * np.exp(-Eg / (R * T)) * np.pow(1.0 + alphaG * L, Lg - 1) * alphaG
+        kG
+        * np.exp(-Eg / (R * T))
+        * np.pow(1.0 + alphaG * L, Lg - 1)
+        * alphaG
+        * np.pow(dC, g)
+    )
+
+
+def calculate_C_sat(T):
+    TC = T - 273.15
+    return (
+        4e-5 * np.pow(TC, 4)
+        - 0.0034 * np.pow(TC, 3)
+        + 0.1024 * np.pow(TC, 2)
+        - 0.6255 * TC
+        + 13.237
     )
 
 
@@ -61,8 +85,6 @@ R = read_param("R", 8.314)
 rho = read_param("rhoCrystal", 1200)
 kv = read_param("kv", np.pi / 6)
 
-assert g == 0, "concentration dependence currently not implemented"
-
 C = 0.6 * rho
 t = 0
 dt = 1
@@ -70,33 +92,35 @@ t_max = 2000
 orders = range(6)
 
 with open(join(dirname, "moments.dat"), "w") as file_out:
-    file_out.write("t\tC\tT")
+    file_out.write("t\tT\tC_sat\tC")
     for order in orders:
         file_out.write(f"\tM{order}")
     file_out.write("\n")
     # output results at t=0
     T = calculate_T(t)
+    C_sat = calculate_C_sat(T)
     L = L0
     f = f0
-    file_out.write(f"{t}\t{C}\t{T}")
+    file_out.write(f"{t}\t{T}\t{C_sat}\t{C}")
     m3old = calculate_moment(L, f, 3)
     for order in orders:
         m = calculate_moment(L, f, order)
-        file_out.write(f"\t{m:e}")
+        file_out.write(f"\t{m:.9e}")
     file_out.write("\n")
-    while t <= t_max:
+    while t < t_max:
         t += dt
         T = calculate_T(t)
-        G = calculate_G(T, L)
-        dGdL = calculate_dG_dL(T, L)
+        C_sat = calculate_C_sat(T)
+        G = calculate_G(T, L, C)
+        dGdL = calculate_dG_dL(T, L, C)
         # method of characteristics
         L += G * dt
         f -= f * dGdL * dt
         m3 = calculate_moment(L, f, 3)
         C -= kv * rho * (m3 - m3old)
-        file_out.write(f"{t}\t{C}\t{T}")
+        file_out.write(f"{t}\t{T}\t{C_sat}\t{C}")
         for order in orders:
             m = calculate_moment(L, f, order)
-            file_out.write(f"\t{m:e}")
+            file_out.write(f"\t{m:.9e}")
         file_out.write("\n")
         m3old = m3
